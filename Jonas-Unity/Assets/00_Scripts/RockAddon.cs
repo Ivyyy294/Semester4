6671945472f4dffa1d0a1da01d3c7cec55b8b4fa
Ivyyy294 <48586445+Ivyyy294@users.noreply.github.com>
2024-04-21 11:41:14 +0200

    :hammer: RockAddon

--- Jonas-Unity/Assets/00_Scripts/RockAddon.cs
+++ Jonas-Unity/Assets/00_Scripts/RockAddon.cs
@@ -2,13 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public class RockSettings
+{
+	public Vector2 levelMinMax;
+	public float angleMax = 45f;
+	public float chance;
+}
+
 public class RockAddon : MonoBehaviour, ITerrainGeneratorAddon
 {
 	public GameObject rockPrefab;
 	public Vector2 levelMinMax;
 
+	[Range (0f, 90f)]
+	public float angleMax;
+
+	[Range (0f, 1f)]
+	public float chance = 1f;
+
 	List <GameObject> rockObjList = new List<GameObject>();
-	Vector2 previousSettings;
+	RockSettings previousSettings = new RockSettings();
 	Mesh previousMesh;
 
     public void Apply (Mesh mesh)
@@ -16,41 +30,47 @@ public class RockAddon : MonoBehaviour, ITerrainGeneratorAddon
 		if (!mesh)
 			return;
 
-		int grassIndex = 0;
+		int rockIndex = 0;
 
 		for (int i = 0; i < mesh.vertexCount; ++i)
 		{
 			Vector3 v = mesh.vertices[i];
+			float angle = Mathf.Abs (Vector3.Angle(mesh.normals[i], Vector3.up));
 
-			if (v.y >= levelMinMax.x && v.y <= levelMinMax.y)
+			if (v.y >= levelMinMax.x && v.y <= levelMinMax.y && angle <= angleMax && chance >= Random.value)
 			{
-				if (grassIndex < rockObjList.Count)
+
+				if (rockIndex < rockObjList.Count)
 				{
-					rockObjList[grassIndex].SetActive (true);
-					rockObjList[grassIndex].transform.position = v;
-					rockObjList[grassIndex].transform.up = mesh.normals[i];
+					rockObjList[rockIndex].SetActive (true);
+					rockObjList[rockIndex].transform.position = v;
+					rockObjList[rockIndex].transform.up = Random.insideUnitSphere;
 				}
 				else
 				{
 					var obj = Instantiate (rockPrefab, v, Quaternion.identity, transform);
-					obj.transform.up = mesh.normals[i];
+					obj.transform.up = Random.insideUnitSphere;
 					rockObjList.Add (obj);
 				}
-				grassIndex++;
+				rockIndex++;
 			}
 		}
 
 		//Disable unused grass
-		for (int i = rockObjList.Count - 1; i > grassIndex; --i)
+		for (int i = rockObjList.Count - 1; i > rockIndex; --i)
 			rockObjList[i].SetActive (false);
 
-		previousSettings = levelMinMax;
+		previousSettings.levelMinMax = levelMinMax;
+		previousSettings.angleMax = angleMax;
+		previousSettings.chance = chance;
 		previousMesh = mesh;
 	}
 
 	private void Update()
 	{
-		if (levelMinMax != previousSettings)
+		if (levelMinMax != previousSettings.levelMinMax
+			|| angleMax != previousSettings.angleMax
+			|| chance != previousSettings.chance)
 			Apply(previousMesh);
 	}
 }
